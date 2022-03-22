6615524a8d8fa71058ac492286e3cb0ca81545e1
Sid009192 <88386638+Sid009192@users.noreply.github.com>
2022-03-22 20:46:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var introsound
+
+
+function preload() {
+ introsound = loadSound("the_game/intro.mp3") 
+}
+
+function setup() {
+    introsound.play()
+}
+ 
+function draw() {
+//   background();
+ 
+}
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
